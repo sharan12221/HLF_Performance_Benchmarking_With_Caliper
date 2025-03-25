9313b8e3e6e4e6d43164acbb29c22a675271d37e
Sharan Wakade <117678416+sharan12221@users.noreply.github.com>
2025-03-25 16:28:55 +0530

    Update UpdateBatchSize.sh

--- UpdateBatchSize.sh
+++ UpdateBatchSize.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # cd ..                      
 #  run this in test-network folder dir
-CHANNEL_NAME=$1
-NEW_BATCH_SIZE=${2:-50}
+CHANNEL_NAME=${1:-mychannel}
+NEW_BATCH_SIZE=${2:-1000}
 
-ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
-CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-CORE_PEER_ADDRESS=localhost:7051
-CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
-CORE_PEER_LOCALMSPID=Org1MSP
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
 set -x
 peer channel fetch config config_block.pb -o localhost:7050 -c $CHANNEL_NAME --tls --cafile $ORDERER_CA
 
@@ -42,3 +47,8 @@ CORE_PEER_ADDRESS=localhost:7050
 CORE_PEER_LOCALMSPID=OrdererMSP
 
 peer channel update -f final_update_in_envelope.pb -c $CHANNEL_NAME -o localhost:7050 --tls --cafile $ORDERER_CA
+
+
+MAXBATCHSIZEPATH=".channel_group.groups.Orderer.values.BatchSize.value.max_message_count"
+
+echo "current batch size is $(jq "$MAXBATCHSIZEPATH" config.json)"
